7670a4e3cf0362d6de083d44da1bc42b0facdddd
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2024-11-25 00:55:04 +0100

    Modified SeekableStream Read mode to request data in large chunks, and only when actually required, as to improve performance when little data is actually read from the stream

---
+++ Sirius.VAF.Extensions.Tests/SeekableStreamTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Moq;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Sirius.VAF {
+	public class SeekableStreamTests {
+		protected ITestOutputHelper Output {
+			get;
+		}
+
+		public SeekableStreamTests(ITestOutputHelper output) {
+			Output = output;
+		}
+
+		[Fact]
+		public void ReadStreamTest() {
+			const int length = 200000;
+			var underlyingData = new byte[length];
+			new Random(0).NextBytes(underlyingData);
+			var underlyingStream = new Mock<Stream>(MockBehavior.Strict);
+			var underlyingPosition = 0;
+			underlyingStream.SetupGet(s => s.CanRead).Returns(true);
+			underlyingStream.SetupGet(s => s.CanWrite).Returns(false);
+			underlyingStream.SetupGet(s => s.CanSeek).Returns(false);
+			underlyingStream.SetupGet(s => s.Length).Returns(underlyingData.Length);
+			underlyingStream.SetupGet(s => s.Position).Returns(() => underlyingPosition);
+			using (var stream = new SeekableStream(underlyingStream.Object, SeekableStream.Mode.Read)) {
+				underlyingStream.Setup(s => s.Read(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>()))
+						.Returns((byte[] buffer, int offset, int count) => {
+							count = Math.Min(count, underlyingData.Length-underlyingPosition);
+							Array.Copy(underlyingData, underlyingPosition, buffer, offset, count);
+							underlyingPosition += count;
+							Output.WriteLine("Requested {0} bytes", count);
+							return count;
+						})
+						.Verifiable();
+				underlyingStream.Verify(s => s.Read(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>()), Times.Never);
+				var actual = new byte[25000];
+				while (underlyingPosition < underlyingData.Length) {
+					var readPosition = (int)stream.Position;
+					var readCount = Math.Min(actual.Length, underlyingData.Length-readPosition);
+					Output.WriteLine("Read {0} bytes...", readCount);
+					Assert.Equal(readCount, stream.Read(actual, 0, actual.Length));
+					Assert.Equal(underlyingData.Skip(readPosition).Take(readCount), actual.Take(readCount));
+					underlyingStream.Verify(s => s.Read(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>()), Times.Exactly((underlyingPosition+(SeekableStream.ChunkSize-1)) / SeekableStream.ChunkSize));
+				}
+				underlyingStream.Setup(s => s.Close())
+						.Verifiable();
+			}
+			Output.WriteLine("Read completed");
+			underlyingStream.Verify(s => s.Close(), Times.Once);
+		}
+	}
+}
--- Sirius.VAF.Extensions/SeekableStream.cs
+++ Sirius.VAF.Extensions/SeekableStream.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,8 @@ using JetBrains.Annotations;
 
 namespace Sirius.VAF {
 	public sealed class SeekableStream: MemoryStream {
+		public const int ChunkSize = 65536; // largest power of 2 below Large Object threshold of 85k
+
 		public enum Mode {
 			Read,
 			Write,
@@ -22,14 +25,14 @@ namespace Sirius.VAF {
 		private readonly Stream underlyingStream;
 		private readonly Mode mode;
 		private bool disposed;
-		private long bytesFlushed = 0;
+		private int bytesProcessed = 0;
 
-		public SeekableStream([NotNull] Stream underlyingStream, Mode mode): base() {
-			this.underlyingStream = underlyingStream ?? throw new ArgumentNullException(nameof(underlyingStream));
-			this.mode = Mode.WriteNoFlush; // for the initial CopyTo we need to be writeable
+		public SeekableStream([NotNull] Stream underlyingStream, Mode mode): base((int)(underlyingStream ?? throw new ArgumentNullException(nameof(underlyingStream))).Length * (mode == Mode.Read ? 1 : 0)) {
+			Debug.Assert(underlyingStream.Position == 0);
+			this.underlyingStream = underlyingStream;
 			if (mode == Mode.Read) {
-				this.underlyingStream.CopyTo(this);
-				Seek(0, SeekOrigin.Begin);
+				this.mode = Mode.WriteNoFlush; // required for the SetLength setup call to succeed
+				base.SetLength(this.underlyingStream.Length);
 			}
 			this.mode = mode;
 		}
@@ -44,7 +47,7 @@ namespace Sirius.VAF {
 			if (mode == Mode.Read) {
 				throw new NotSupportedException();
 			}
-			if (value < bytesFlushed) {
+			if (value < bytesProcessed) {
 				throw new InvalidOperationException("Cannot set length before the flushed position of the stream");
 			}
 			base.SetLength(value);
@@ -60,15 +63,11 @@ namespace Sirius.VAF {
 		}
 
 		private void FlushInternal() {
-			if (bytesFlushed < Length) {
-				var position = Position;
-				try {
-					Seek(bytesFlushed, SeekOrigin.Begin);
-					CopyTo(underlyingStream);
-					bytesFlushed = Position;
-				} finally {
-					Seek(position, SeekOrigin.Begin);
-				}
+			var buffer = GetBuffer();
+			while (bytesProcessed < Length) {
+				var count = Math.Min((int)Length, bytesProcessed-bytesProcessed % ChunkSize+ChunkSize)-bytesProcessed;
+				underlyingStream.Write(buffer, bytesProcessed, count);
+				bytesProcessed += count;
 			}
 		}
 
@@ -76,10 +75,35 @@ namespace Sirius.VAF {
 			if (mode == Mode.Read) {
 				throw new NotSupportedException();
 			}
-			if (Position < bytesFlushed) {
+			if (Position < bytesProcessed) {
 				throw new InvalidOperationException("Cannot modify the flushed position of the stream");
 			}
 			base.Write(buffer, offset, count);
+		}
+
+		public override int Read(byte[] buffer, int offset, int count) {
+			EnsureDataAvailable((int)Math.Min(Position+count, Length));
+			return base.Read(buffer, offset, count);
+		}
+
+		public override byte[] GetBuffer() {
+			EnsureDataAvailable((int)Length);
+			return base.GetBuffer();
+		}
+
+		private void EnsureDataAvailable(int length) {
+			if (mode != Mode.Read || length <= bytesProcessed) {
+				return;
+			}
+			Debug.Assert(length <= Length);
+			var buffer = base.GetBuffer();
+			do {
+				var bytesRead = underlyingStream.Read(buffer, bytesProcessed, ChunkSize);
+				if (bytesRead == 0) {
+					throw new InvalidOperationException("Unexpected end of underlying stream");
+				}
+				bytesProcessed += bytesRead;
+			} while (bytesProcessed < length);
 		}
 
 		protected override void Dispose(bool disposing) {
