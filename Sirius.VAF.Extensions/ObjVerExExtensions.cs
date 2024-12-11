c0ab584ee9064170f2ff80f31a3551a2c1c6754c
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2024-12-11 14:05:23 +0100

    Added HasAnyClass extension method

--- Sirius.VAF.Extensions/ObjVerExExtensions.cs
+++ Sirius.VAF.Extensions/ObjVerExExtensions.cs
@@ -87,7 +87,7 @@ namespace Sirius.VAF {
 			return result;
 		}
 
-		public static bool UserHasPermissionTo(this ObjVerEx that, int userId, MFPermissionsExpressionType permission, MFConditionType condition = MFConditionType.MFConditionTypeEqual) {
+		public static bool UserHasPermissionTo([NotNull] this ObjVerEx that, int userId, MFPermissionsExpressionType permission, MFConditionType condition = MFConditionType.MFConditionTypeEqual) {
 			return new MFSearchBuilder(that.Vault)
 					.ObjType(that.ObjID.Type)
 					.ObjectId(that.ObjID.ID)
@@ -96,53 +96,53 @@ namespace Sirius.VAF {
 					.Count > 0;
 		}
 
-		public static bool VisibleTo(this ObjVerEx that, int userId) {
+		public static bool VisibleTo([NotNull] this ObjVerEx that, int userId) {
 			return that.UserHasPermissionTo(userId, MFPermissionsExpressionType.MFVisibleTo);
 		}
 
-		public static bool EditableBy(this ObjVerEx that, int userId) {
+		public static bool EditableBy([NotNull] this ObjVerEx that, int userId) {
 			return that.UserHasPermissionTo(userId, MFPermissionsExpressionType.MFEditableBy);
 		}
 
-		public static bool DeletableBy(this ObjVerEx that, int userId) {
+		public static bool DeletableBy([NotNull] this ObjVerEx that, int userId) {
 			return that.UserHasPermissionTo(userId, MFPermissionsExpressionType.MFDeletableBy);
 		}
 
-		public static bool TryGetPropertyWithValue(this ObjVerEx that, MFIdentifier property, out PropertyValue propVal) {
+		public static bool TryGetPropertyWithValue([NotNull] this ObjVerEx that, [NotNull] MFIdentifier property, out PropertyValue propVal) {
 			return that.TryGetProperty(property, out propVal) && !(propVal.Value.IsEmpty() || propVal.Value.IsNULL() || propVal.Value.IsUninitialized());
 		}
 
-		public static string GetPropertyDisplayValue(this ObjVerEx that, MFIdentifier property, string defaultText = "") {
+		public static string GetPropertyDisplayValue([NotNull] this ObjVerEx that, [NotNull] MFIdentifier property, string defaultText = "") {
 			return that.TryGetPropertyWithValue(property, out var propVal)
 					? propVal.Value.DisplayValue
 					: defaultText;
 		}
 
-		public static string GetPropertyTextEx(this ObjVerEx that, MFIdentifier property, bool localized, bool longDateFormat, bool noSeconds, bool numericValueAsKilobytes, bool allowDigitGrouping, string defaultText = "") {
+		public static string GetPropertyTextEx([NotNull] this ObjVerEx that, [NotNull] MFIdentifier property, bool localized, bool longDateFormat, bool noSeconds, bool numericValueAsKilobytes, bool allowDigitGrouping, string defaultText = "") {
 			return that.TryGetPropertyWithValue(property, out var propVal)
 					? propVal.GetValueAsTextEx(localized, true, false, longDateFormat, noSeconds, numericValueAsKilobytes, allowDigitGrouping)
 					: defaultText;
 		}
 
-		public static string GetPropertyUnlocalizedText(this ObjVerEx that, MFIdentifier property, string defaultText = "") {
+		public static string GetPropertyUnlocalizedText([NotNull] this ObjVerEx that, [NotNull] MFIdentifier property, string defaultText = "") {
 			return that.TryGetPropertyWithValue(property, out var propVal)
 					? propVal.GetValueAsUnlocalizedText()
 					: defaultText;
 		}
 
-		public static string GetPropertyLocalizedText(this ObjVerEx that, MFIdentifier property, string defaultText = "") {
+		public static string GetPropertyLocalizedText([NotNull] this ObjVerEx that, [NotNull] MFIdentifier property, string defaultText = "") {
 			return that.TryGetPropertyWithValue(property, out var propVal)
 					? propVal.GetValueAsLocalizedText()
 					: defaultText;
 		}
 
-		public static string GetPropertyLocalizedTextEx(this ObjVerEx that, MFIdentifier property, bool allowDigitGrouping, string defaultText = "") {
+		public static string GetPropertyLocalizedTextEx([NotNull] this ObjVerEx that, [NotNull] MFIdentifier property, bool allowDigitGrouping, string defaultText = "") {
 			return that.TryGetPropertyWithValue(property, out var propVal)
 					? propVal.GetValueAsLocalizedTextEx(allowDigitGrouping)
 					: defaultText;
 		}
 
-		public static Lookup ToSpecificVersionLookup(this ObjVerEx that) {
+		public static Lookup ToSpecificVersionLookup([NotNull] this ObjVerEx that) {
 			return new Lookup() {
 					ObjectType = that.Type,
 					Item = that.ID,
@@ -150,7 +150,7 @@ namespace Sirius.VAF {
 			};
 		}
 
-		public static Lookup ToLatestVersionLookup(this ObjVerEx that) {
+		public static Lookup ToLatestVersionLookup([NotNull] this ObjVerEx that) {
 			return new Lookup() {
 					ObjectType = that.Type,
 					Item = that.ID,
@@ -158,8 +158,12 @@ namespace Sirius.VAF {
 			};
 		}
 
-		public static string GetUid(this ObjVerEx that) {
+		public static string GetUid([NotNull] this ObjVerEx that) {
 			return that.Info.ObjectGUID.Trim('{', '}');
+		}
+
+		public static bool HasAnyClass([NotNull] this ObjVerEx that, params MFIdentifier[] classes) {
+			return (classes != null) && classes.Any(that.HasClass);
 		}
 	}
 }
